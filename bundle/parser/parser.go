4bf1b07d1aa20860ce15fdfcb93d76393602e98b
zengxiaocan <zengxiaocan@joyy.com>
2025-05-16 18:22:09 +0800

    feat: 🎸 修改命名

--- bundle/parser/parser.go
+++ bundle/parser/parser.go
@@ -7,33 +7,33 @@ import (
 	"github.com/Zzzen/typescript-go/use-at-your-own-risk/ast"
 )
 
-type BundleResult struct {
+type ParserResult struct {
 	ImportDeclarations    []ImportDeclarationResult
 	InterfaceDeclarations []InterfaceDeclarationResult
 	TypeDeclarations      []TypeDeclarationResult
 }
 
-func NewBundleResult() BundleResult {
-	return BundleResult{
+func NewBundleResult() ParserResult {
+	return ParserResult{
 		ImportDeclarations:    []ImportDeclarationResult{},
 		InterfaceDeclarations: []InterfaceDeclarationResult{},
 		TypeDeclarations:      []TypeDeclarationResult{},
 	}
 }
 
-func (br *BundleResult) AddImportDeclaration(idr *ImportDeclarationResult) {
-	br.ImportDeclarations = append(br.ImportDeclarations, *idr)
+func (pr *ParserResult) AddImportDeclaration(idr *ImportDeclarationResult) {
+	pr.ImportDeclarations = append(pr.ImportDeclarations, *idr)
 }
 
-func (br *BundleResult) AddInterfaceDeclaration(inter *InterfaceDeclarationResult) {
-	br.InterfaceDeclarations = append(br.InterfaceDeclarations, *inter)
+func (pr *ParserResult) AddInterfaceDeclaration(inter *InterfaceDeclarationResult) {
+	pr.InterfaceDeclarations = append(pr.InterfaceDeclarations, *inter)
 }
 
-func (br *BundleResult) addTypeDeclaration(tr *TypeDeclarationResult) {
-	br.TypeDeclarations = append(br.TypeDeclarations, *tr)
+func (pr *ParserResult) addTypeDeclaration(tr *TypeDeclarationResult) {
+	pr.TypeDeclarations = append(pr.TypeDeclarations, *tr)
 }
 
-func Traverse(filePath string) BundleResult {
+func Traverse(filePath string) ParserResult {
 	sourceCode, err := utils.ReadFileContent(filePath)
 	if err != nil {
 		fmt.Printf("读取文件失败: %v\n", err)
@@ -65,25 +65,25 @@ func Traverse(filePath string) BundleResult {
 		}
 	}
 
-	// 解析 Interface 中的 type
-	for _, inter := range bundle.InterfaceDeclarations {
-		fmt.Printf("Name: %s\n", inter.Name)
-		fmt.Printf("Raw: %s\n", inter.Raw)
-		for _, ref := range inter.Reference {
-			fmt.Printf("Reference: %s, %v, %b \n", ref.Name, ref.Location, ref.IsExtend)
-		}
-		fmt.Print("\n\n\n")
-	}
+	// // 解析 Interface 中的 type
+	// for _, inter := range bundle.InterfaceDeclarations {
+	// 	fmt.Printf("Name: %s\n", inter.Name)
+	// 	fmt.Printf("Raw: %s\n", inter.Raw)
+	// 	for _, ref := range inter.Reference {
+	// 		fmt.Printf("Reference: %s, %v, %b \n", ref.Name, ref.Location, ref.IsExtend)
+	// 	}
+	// 	fmt.Print("\n\n\n")
+	// }
 
-	// 解析 Type 中的 type
-	for _, tr := range bundle.TypeDeclarations {
-		fmt.Printf("Name: %s\n", tr.Name)
-		fmt.Printf("Raw: %s\n", tr.Raw)
-		for _, ref := range tr.Reference {
-			fmt.Printf("Reference: %s, %v, %b \n", ref.Name, ref.Location, ref.IsExtend)
-		}
-		fmt.Print("\n\n\n")
-	}
+	// // 解析 Type 中的 type
+	// for _, tr := range bundle.TypeDeclarations {
+	// 	fmt.Printf("Name: %s\n", tr.Name)
+	// 	fmt.Printf("Raw: %s\n", tr.Raw)
+	// 	for _, ref := range tr.Reference {
+	// 		fmt.Printf("Reference: %s, %v, %b \n", ref.Name, ref.Location, ref.IsExtend)
+	// 	}
+	// 	fmt.Print("\n\n\n")
+	// }
 
 	return bundle
 }
